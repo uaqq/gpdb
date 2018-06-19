8fc013d386a9897161a171a65f90d216045452f2
Ivan Leskin <leskin.in@phystech.edu>
2018-06-19 19:44:46 +0300

    Add unit test in pgport
    Add unit test for libpgport (namely, CRC32C calculation).
    
    The test calls the macroses defined in pg_crc32c.h to calculate the CRC32C sum and checks if it is equal to a real one.
    
    These tests allow to test any CRC32C calculation implementation, given that GPDB is built on the machine that supports such CRC32C calculation mechanism.

---
+++ src/port/test/pg_crc32c_test.c
@@ -0,0 +1,32 @@
+#include "cmockery.h"
+#include "port/pg_crc32c.h"
+
+
+void
+test__crc32_correctness(void **state)
+{
+    const unsigned data_length = 5;
+    const char data[data_length] = {(char)0x6, (char)0x7, (char)0x9, (char)0x3, (char)0x2};
+    const unsigned long correct_result = 0xbbef7758;
+
+    pg_crc32c result = 0;
+
+    INIT_CRC32C(result);
+    COMP_CRC32C(result, data, data_length);
+    FIN_CRC32C(result);
+
+    assert_true(EQ_CRC32C(result, correct_result));
+}
+
+int
+main(int argc, char* argv[])
+{
+	cmockery_parse_arguments(argc, argv);
+
+	const UnitTest tests[] =
+	{
+		unit_test(test__crc32_correctness)
+	};
+
+	return run_tests(tests);
+}
