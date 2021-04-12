26f1ec5e2cbe8b6c85e681a9dc62cfb2efa590ae
Ivan Leskin <leskin.in@arenadata.io>
2021-04-12 12:28:06 +0300

    Modify WAL save process
    * Save WALs to pg_wal directory of pg_basebackup'ed replica. Change
    restore_command to 'ls' in order to achieve the behaviour expected by
    PostgreSQL.
    
    * Rename the last .partial WAL in replica's pg_wal directory to
    non-partial one. That file is obtained from the primary after recovery;
    however, pg_walreceive does not rename it to non-partial even when the
    segment (from which the WAL is received) is stopped (in "smart" mode!)

--- src/test/receivewal/receivewal.sh
+++ src/test/receivewal/receivewal.sh
@@ -23,10 +23,7 @@ REPLICA_COORDINATOR_DBID=10
 
 REPLICA_PRIMARY1_DBID=2
 REPLICA_PRIMARY1_DIR=${DIR_TEMP}/replica_p1
-REPLICA_PRIMARY1_ARCHIVE=${DIR_TEMP}/replica_p1_archive
-
-REPLICA_MIRROR1_DIR=${DIR_TEMP}/replica_m1
-REPLICA_MIRROR1_PORT=7005
+REPLICA_PRIMARY1_ARCHIVE=${REPLICA_PRIMARY1_DIR}/pg_wal
 
 
 RECEIVEWAL_PID=-1
@@ -50,8 +47,7 @@ echo "(2) Launching pg_receivewal for WALs from primary1"
 for r in PRIMARY1; do
     PORT_VAR=${r}_PORT
     REPLICA_ARCHIVE_VAR=REPLICA_${r}_ARCHIVE
-    mkdir ${!REPLICA_ARCHIVE_VAR}
-    pg_receivewal -h localhost -p ${!PORT_VAR} -D ${!REPLICA_ARCHIVE_VAR} >${DIR_TEMP}/receivewal.log 2>&1 &
+    pg_receivewal -h localhost -p ${!PORT_VAR} -D ${!REPLICA_ARCHIVE_VAR} >>${DIR_TEMP}/receivewal.log 2>&1 &
     RECEIVEWAL_PID=$!
 done
 echo "(2) FINISHED"
@@ -63,6 +59,7 @@ echo "(3) FINISHED"
 echo "(4) Killing primary1"
 PRIMARY1_PID=$(ps ax | grep postgres | grep "${PRIMARY1_DIR}" | awk '{print $1}')
 kill -9 ${PRIMARY1_PID}
+psql postgres -ea -c 'CHECKPOINT;'
 gpstate
 echo "(4) FINISHED"
 
@@ -70,7 +67,8 @@ echo "(5) Restarting pg_receivewal: consume WAL from mirror1"
 for rn in 1; do
     kill -s SIGINT ${RECEIVEWAL_PID}
     PORT_MIRROR_VAR=MIRROR${rn}_PORT
-    pg_receivewal -h localhost -p ${!PORT_MIRROR_VAR} -D ${!REPLICA_ARCHIVE_VAR} >${DIR_TEMP}/receivewal.log 2>&1 &
+    REPLICA_ARCHIVE_VAR=REPLICA_PRIMARY${rn}_ARCHIVE
+    pg_receivewal -h localhost -p ${!PORT_MIRROR_VAR} -D ${!REPLICA_ARCHIVE_VAR} >>${DIR_TEMP}/receivewal.log 2>&1 &
     RECEIVEWAL_PID=$!
     ls -ltrh ${!REPLICA_ARCHIVE_VAR}
 done
@@ -89,7 +87,8 @@ echo "(8) Restarting pg_receivewal: consume WAL from primary1"
 for rn in 1; do
     kill -s SIGINT ${RECEIVEWAL_PID}
     PORT_PRIMARY_VAR=PRIMARY${rn}_PORT
-    pg_receivewal -h localhost -p ${!PORT_PRIMARY_VAR} -D ${!REPLICA_ARCHIVE_VAR} >${DIR_TEMP}/receivewal.log 2>&1 &
+    REPLICA_ARCHIVE_VAR=REPLICA_PRIMARY${rn}_ARCHIVE
+    pg_receivewal -h localhost -p ${!PORT_PRIMARY_VAR} -D ${!REPLICA_ARCHIVE_VAR} >>${DIR_TEMP}/receivewal.log 2>&1 &
     RECEIVEWAL_PID=$!
     ls -ltrh ${!REPLICA_ARCHIVE_VAR}
 done
@@ -106,17 +105,31 @@ for rn in 1; do
 done
 echo "(10) FINISHED"
 
-echo "(11) Recovering segment"
+echo "(11) Dirty modifications of WAL files obtained by pg_receivewal"
+for r in PRIMARY1; do
+    REPLICA_ARCHIVE_VAR=REPLICA_${r}_ARCHIVE
+    ls -ltrh ${!REPLICA_ARCHIVE_VAR}
+    FILE_TO_RENAME=$(ls -1 ${!REPLICA_ARCHIVE_VAR} | tail -n 3 | head -n 1)
+    FILE_TO_RENAME_WITH_PATH="${!REPLICA_ARCHIVE_VAR}/${FILE_TO_RENAME}"
+    RESULTING_FILE="${FILE_TO_RENAME%.*}"
+    RESULTING_FILE_WITH_PATH="${!REPLICA_ARCHIVE_VAR}/${RESULTING_FILE}"
+    mv ${FILE_TO_RENAME_WITH_PATH} ${RESULTING_FILE_WITH_PATH}
+    ls -ltrh ${!REPLICA_ARCHIVE_VAR}
+done
+echo "(11) FINISHED"
+
+echo "(12) Recovering segment"
 for r in PRIMARY1; do
     REPLICA_DIR_VAR=REPLICA_${r}_DIR
     REPLICA_ARCHIVE_VAR=REPLICA_${r}_ARCHIVE
-    cp ${!REPLICA_DIR_VAR}/postgresql.conf ${!REPLICA_DIR_VAR}/postgresql.conf.backup
+    cp ${!REPLICA_DIR_VAR}/postgresql.conf ${!REPLICA_DIR_VAR}/postgresql.backup.conf
     echo "
-restore_command = 'cp ${!REPLICA_ARCHIVE_VAR}/%f %p'
+restore_command = 'ls ${!REPLICA_ARCHIVE_VAR}/%f > /dev/null 2>&1'
 recovery_target_name = 'rp'
-recovery_target_action = 'promote'
+recovery_target_action = 'pause'
+hot_standby = on
 " >> ${!REPLICA_DIR_VAR}/postgresql.conf
     touch ${!REPLICA_DIR_VAR}/recovery.signal
     pg_ctl start -D ${!REPLICA_DIR_VAR}
 done
-echo "(11) FINISHED"
+echo "(12) FINISHED"
