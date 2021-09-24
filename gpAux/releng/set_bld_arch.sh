1cab3aa07c56de200953d8885ce821ea860e35dd
Artem Eluferev <ae@arenadata.io>
2021-09-24 11:11:57 +0300

    ADBDEV-1994 add altlinux to set_bld_arch.sh

--- gpAux/releng/set_bld_arch.sh
+++ gpAux/releng/set_bld_arch.sh
@@ -4,7 +4,11 @@
 
 case "`uname -s`" in
     Linux)
-    if [ -f /etc/redhat-release ]; then
+    . /etc/os-release;
+    if [ ${ID} == "altlinux" ]; then
+        BLD_ARCH_HOST="$(. /etc/os-release; echo ${ID}${VERSION_ID}_$(uname -m))"
+    fi
+    if [ -z "${BLD_ARCH_HOST}" -a -f /etc/redhat-release ]; then
         case "`cat /etc/redhat-release`" in
             *)
             BLD_ARCH_HOST="rhel`cat /etc/redhat-release | sed -e 's/CentOS Linux/RedHat/' -e 's/Red Hat Enterprise Linux/RedHat/' -e 's/WS//' -e 's/Server//' -e 's/Client//' | awk '{print $3}' | awk -F. '{print $1}'`_`uname -p | sed -e s/i686/x86_32/`"
