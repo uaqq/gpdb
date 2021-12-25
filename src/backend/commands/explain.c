fa4da38fdee8d037e03f0ea00cf2963d301133b8
hughcapet <bungina@arenadata.io>
2021-12-25 12:04:14 +0700

    Add condition for scaleFactor definition based on motionNode presence

--- src/backend/commands/explain.c
+++ src/backend/commands/explain.c
@@ -1247,6 +1247,10 @@ ExplainNode(PlanState *planstate, List *ancestors,
 			 */
 			scaleFactor = CdbPathLocus_NumSegments(*(plan->flow));
 		}
+		else if (plan->flow == NULL && plan->motionNode == NULL)
+		{
+			scaleFactor = 1.0;
+		}
 		else
 		{
 			/*
