9fc4135221c138f6a15976f0a4eb3e72a40ed60b
Maksim Milyutin <mma@arenadata.io>
2022-01-17 20:03:38 +0300

    Keep the locks on partitions until end of transaction
    Commit fa287d0 had introduced early lock releasing on partitions within
    operations with partitioned parent table. This was done to save memory
    in lock manager I suppose. But such behavior incurs some issues, e.g.,
    possible segfault when we perform drop of partitioned table and
    concurrent `pg_total_relation_size()` call on some AO partition.
    
    Obviously, we have to restore full locking for inheritance hierarchy as
    it's done in vanilla PostgreSQL. To eliminate exhaustion of lock memory
    we need to increase max_locks_per_transactions setting. Memory is cheap,
    and if we're working with tens of thousands of partitions, we can afford
    reserving some memory for the lock manager.
    
    Partially for DML operations the problem of partitions locking was
    resolved in commit 99fbc8e. Therefore in current work we have to complete
    that fix to support partitions locking for DDL operations and other cases.
    And it's in fact backport of similar commit to master 86ded36 with small
    additions. Also there have been added test scenario to reproduce noted
    above segfault case and related assert check inside
    `calculate_table_size()` function.
    
    Backport of PR https://github.com/greenplum-db/gpdb/pull/10468.
    Fixes https://github.com/greenplum-db/gpdb/issues/12962

--- src/backend/commands/indexcmds.c
+++ src/backend/commands/indexcmds.c
@@ -434,7 +434,6 @@ DefineIndex(Oid relationId,
 	LOCKTAG		heaplocktag;
 	LOCKMODE	lockmode;
 	Snapshot	snapshot;
-	bool		need_longlock = true;
 	bool		shouldDispatch;
 	int			i;
 
@@ -820,14 +819,6 @@ DefineIndex(Oid relationId,
 		 */
 	}
 
-	if (rel_needs_long_lock(RelationGetRelid(rel)))
-		need_longlock = true;
-	/* if this is a concurrent build, we must lock you long time */
-	else if (stmt->concurrent)
-		need_longlock = true;
-	else
-		need_longlock = false;
-
 	/*
 	 * A valid stmt->oldNode implies that we already have a built form of the
 	 * index.  The caller should also decline any index build.
@@ -895,10 +886,7 @@ DefineIndex(Oid relationId,
 			for (i = 0; i < numberOfAttributes; i++)
 				opfamOids[i] = get_opclass_family(classObjectId[i]);
 
-			if (need_longlock)
-				heap_close(rel, NoLock);
-			else
-				heap_close(rel, lockmode);
+			heap_close(rel, NoLock);
 
 			/*
 			 * For each partition, scan all existing indexes; if one matches
@@ -1086,10 +1074,7 @@ DefineIndex(Oid relationId,
 		 */
 		else
 		{
-			if (need_longlock)
-				heap_close(rel, NoLock);
-			else
-				heap_close(rel, lockmode);
+			heap_close(rel, NoLock);
 		}
 		/*
 		 * Indexes on partitioned tables are not themselves built, so we're
@@ -1101,10 +1086,7 @@ DefineIndex(Oid relationId,
 	if (!concurrent)
 	{
 		/* Close the heap and we're done, in the non-concurrent case */
-		if (need_longlock)
-			heap_close(rel, NoLock);
-		else
-			heap_close(rel, lockmode);
+		heap_close(rel, NoLock);
 		return indexRelationId;
 	}
 
@@ -1122,10 +1104,7 @@ DefineIndex(Oid relationId,
 	/* save lockrelid and locktag for below, then close rel */
 	heaprelid = rel->rd_lockInfo.lockRelId;
 	SET_LOCKTAG_RELATION(heaplocktag, heaprelid.dbId, heaprelid.relId);
-	if (need_longlock)
-		heap_close(rel, NoLock);
-	else
-		heap_close(rel, lockmode);
+	heap_close(rel, NoLock);
 
 	/*
 	 * For a concurrent build, it's important to make the catalog entries
