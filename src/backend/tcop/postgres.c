1aac1a999af2150a367385c3ae20a2e2fd5a4475
Maksim Milyutin <mma@arenadata.io>
2022-03-02 14:30:46 +0300

    Add comment for CancelRequested() function

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -6022,6 +6022,9 @@ disable_client_wait_timeout_interrupt(void)
 		DisableClientWaitTimeoutInterrupt();
 }
 
+/*
+ * Whether request on cancel or termination have arrived?
+ */
 inline bool
 CancelRequested() {
 	return InterruptPending && (ProcDiePending || QueryCancelPending);
