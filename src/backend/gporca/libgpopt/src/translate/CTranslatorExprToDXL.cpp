1051acd755ce82e4b613de5920e6497861638a4b
Georgy Shelkovy <g.shelkovy@arenadata.io>
2022-11-15 09:16:15 +0500

    ADBDEV-3098-5: ORCA produces bogus plan for queries with CTE during handling distribution for Sequence children

--- src/backend/gporca/libgpopt/src/translate/CTranslatorExprToDXL.cpp
+++ src/backend/gporca/libgpopt/src/translate/CTranslatorExprToDXL.cpp
@@ -8249,13 +8249,13 @@ CTranslatorExprToDXL::GetInputSegIdsArray(CExpression *pexprMotion)
 		// previously made CTEProducerConsumerLocality check.
 		// Related to: https://github.com/greenplum-db/gpdb/issues/13039
 
-		if (CUtils::hasUnpairedCTEConsumer(m_mp, pexprMotion))
+		/*if (CUtils::hasUnpairedCTEConsumer(m_mp, pexprMotion))
 		{
 			GPOS_RAISE(
 				gpdxl::ExmaDXL, gpdxl::ExmiExpr2DXLUnsupportedFeature,
 				GPOS_WSZ_LIT(
 					"CTE Consumer without the appropriate CTE Producer under a duplicate-hazard motion or a tainted replicated node"));
-		}
+		}*/
 
 		IntPtrArray *pdrgpi = GPOS_NEW(m_mp) IntPtrArray(m_mp);
 		INT iSegmentId = *((*m_pdrgpiSegments)[0]);
