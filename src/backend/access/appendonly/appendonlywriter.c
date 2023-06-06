4a1c5cb872609efaa48f3d755c91af22936a54d6
Georgy Shelkovy <g.shelkovy@arenadata.io>
2023-06-06 08:46:54 +0500

    fix

--- src/backend/access/appendonly/appendonlywriter.c
+++ src/backend/access/appendonly/appendonlywriter.c
@@ -765,6 +765,7 @@ RegisterSegnoForCompactionDrop(Oid relid, List *compactedSegmentFileList, bool e
 
 		if (list_member_int(compactedSegmentFileList, i))
 		{
+			Snapshot	snapshot = GetActiveSnapshot();
 			if (!exclusive && (
 				(cutoff_xid == GetTopTransactionId() && GetTopTransactionId() == snapshot->xmin && snapshot->xmin == snapshot->xmax && TransactionIdPrecedes(segfilestat->latestWriteXid, cutoff_xid)) ||
 				(cutoff_xid == snapshot->xmin && snapshot->xmax == GetTopTransactionId() && TransactionIdFollows(segfilestat->latestWriteXid, cutoff_xid)) ||
