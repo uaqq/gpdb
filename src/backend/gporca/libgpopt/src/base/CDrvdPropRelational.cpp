3410ed70caa6e951655361cb89a5cdf36f2b1622
Georgy Shelkovy <g.shelkovy@arenadata.io>
2023-11-17 10:23:48 +0500

    optimize

--- src/backend/gporca/libgpopt/src/base/CDrvdPropRelational.cpp
+++ src/backend/gporca/libgpopt/src/base/CDrvdPropRelational.cpp
@@ -355,27 +355,27 @@ CDrvdPropRelational::DeriveOutputColumns(CExpressionHandle &exprhdl,
 		m_pcrsOutput = popLogical->DeriveOutputColumns(m_mp, exprhdl);
 	}
 
-	if (!include_unused)
+	if (include_unused)
 	{
-		CColRefSet *pcrsOutput = GPOS_NEW(m_mp) CColRefSet(m_mp);
-		CColRefSetIter crsi(*m_pcrsOutput);
-
-		while (crsi.Advance())
-		{
-			// We want to limit the output columns to only those which are referenced in the query
-			// We will know the entire list of columns which are referenced in the query only after
-			// translating the entire DXL to an expression. Hence we should not limit the output columns
-			// before we have processed the entire DXL.
-			if (CColRef::EUnused != crsi.Pcr()->GetUsage())
-			{
-				pcrsOutput->Include(crsi.Pcr());
-			}
-		}
-
-		return pcrsOutput;
+		return m_pcrsOutput;
 	}
 
-	return m_pcrsOutput;
+	CColRefSet *pcrsOutput = GPOS_NEW(m_mp) CColRefSet(m_mp);
+	CColRefSetIter crsi(*m_pcrsOutput);
+
+	while (crsi.Advance())
+	{
+		// We want to limit the output columns to only those which are referenced in the query
+		// We will know the entire list of columns which are referenced in the query only after
+		// translating the entire DXL to an expression. Hence we should not limit the output columns
+		// before we have processed the entire DXL.
+		if (CColRef::EUnused != crsi.Pcr()->GetUsage())
+		{
+			pcrsOutput->Include(crsi.Pcr());
+		}
+	}
+
+	return pcrsOutput;
 }
 
 // outer references
