44b123c15f14a1c0916f48e99ad362b6f3f8b87e
Denis Kovalev <d.kovalev@arenadata.io>
2024-02-13 22:29:55 +0600

    Make SRF __gp_aoblkdir materialized

--- src/backend/access/appendonly/appendonly_blkdir_udf.c
+++ src/backend/access/appendonly/appendonly_blkdir_udf.c
@@ -40,9 +40,6 @@ Datum gp_aoblkdir(PG_FUNCTION_ARGS);
 Datum
 gp_aoblkdir(PG_FUNCTION_ARGS)
 {
-	Oid       	aoRelOid = PG_GETARG_OID(0);
-	HeapTuple 	tuple;
-
 	typedef struct Context
 	{
 		Relation 				aorel;
@@ -53,41 +50,63 @@ gp_aoblkdir(PG_FUNCTION_ARGS)
 		Relation				blkdirrel;
 	} Context;
 
-	FuncCallContext *funcctx;
-	Context			*context;
+	Context    *context;
+	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
+	Oid			aoRelOid = PG_GETARG_OID(0);
+	Tuplestorestate *tupstore;
+	MemoryContext per_query_ctx;
+	TupleDesc     tupdesc;
+	MemoryContext oldcontext;
 
-	if (SRF_IS_FIRSTCALL())
-	{
-		TupleDesc     tupdesc;
-		MemoryContext oldcontext;
-		Snapshot      sst;
-		Oid           blkdirrelid;
+	/* check to see if caller supports us returning a tuplestore */
+	if (rsinfo == NULL || !IsA(rsinfo, ReturnSetInfo))
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("set-valued function called in context that cannot accept a set")));
 
-		/* create a function context for cross-call persistence */
-		funcctx = SRF_FIRSTCALL_INIT();
+	if (!(rsinfo->allowedModes & SFRM_Materialize))
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("materialize mode required, but it is not " \
+						"allowed in this context")));
 
-		/*
-		 * switch to memory context appropriate for multiple function calls
-		 */
-		oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+	/* Build a tuple descriptor for our result type */
 
-		/* build tupdesc for result tuples */
+	/* build tupdesc for result tuples */
 		tupdesc = CreateTemplateTupleDesc(7);
-		TupleDescInitEntry(tupdesc, (AttrNumber) 1, "tupleid",
-						   TIDOID, -1, 0);
-		TupleDescInitEntry(tupdesc, (AttrNumber) 2, "segno",
-						   INT4OID, -1, 0);
-		TupleDescInitEntry(tupdesc, (AttrNumber) 3, "columngroup_no",
-						   INT4OID, -1, 0);
-		TupleDescInitEntry(tupdesc, (AttrNumber) 4, "entry_no",
-						   INT4OID, -1, 0);
-		TupleDescInitEntry(tupdesc, (AttrNumber) 5, "first_row_no",
-						   INT8OID, -1, 0);
-		TupleDescInitEntry(tupdesc, (AttrNumber) 6, "file_offset",
-						   INT8OID, -1, 0);
-		TupleDescInitEntry(tupdesc, (AttrNumber) 7, "row_count",
-						   INT8OID, -1, 0);
-		funcctx->tuple_desc = BlessTupleDesc(tupdesc);
+	TupleDescInitEntry(tupdesc, (AttrNumber) 1, "tupleid",
+					   TIDOID, -1, 0);
+	TupleDescInitEntry(tupdesc, (AttrNumber) 2, "segno",
+					   INT4OID, -1, 0);
+	TupleDescInitEntry(tupdesc, (AttrNumber) 3, "columngroup_no",
+					   INT4OID, -1, 0);
+	TupleDescInitEntry(tupdesc, (AttrNumber) 4, "entry_no",
+					   INT4OID, -1, 0);
+	TupleDescInitEntry(tupdesc, (AttrNumber) 5, "first_row_no",
+					   INT8OID, -1, 0);
+	TupleDescInitEntry(tupdesc, (AttrNumber) 6, "file_offset",
+					   INT8OID, -1, 0);
+	TupleDescInitEntry(tupdesc, (AttrNumber) 7, "row_count",
+					   INT8OID, -1, 0);
+
+	/*
+	 * We don't require any special permission to see this function's data
+	 * because nothing should be sensitive. The most critical being the slot
+	 * name, which shouldn't contain anything particularly sensitive.
+	 */
+
+	per_query_ctx = rsinfo->econtext->ecxt_per_query_memory;
+	oldcontext = MemoryContextSwitchTo(per_query_ctx);
+
+	tupstore = tuplestore_begin_heap(true, false, work_mem);
+
+	rsinfo->returnMode = SFRM_Materialize;
+	rsinfo->setResult = tupstore;
+	rsinfo->setDesc = tupdesc;
+
+	{
+		Snapshot	sst;
+		Oid			blkdirrelid;
 
 		/* initialize Context for SRF */
 		context = (Context *) palloc0(sizeof(Context));
@@ -95,7 +114,7 @@ gp_aoblkdir(PG_FUNCTION_ARGS)
 		if (!RelationStorageIsAO(context->aorel))
 			ereport(ERROR,
 					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-						errmsg("function not supported on non append-optimized relation")));
+					 errmsg("function not supported on non append-optimized relation")));
 		sst = GetLatestSnapshot();
 		GetAppendOnlyEntryAuxOids(context->aorel,
 								  NULL, &blkdirrelid,
@@ -115,96 +134,98 @@ gp_aoblkdir(PG_FUNCTION_ARGS)
 		context->currMinipage.minipage = palloc0(minipage_size(NUM_MINIPAGE_ENTRIES));
 		context->currMinipageValid = false;
 		context->currMinipageEntryIdx = -1;
-		funcctx->user_fctx = (void *) context;
 
-		MemoryContextSwitchTo(oldcontext);
 	}
 
-	funcctx = SRF_PERCALL_SETUP();
-	context = (Context *) funcctx->user_fctx;
+	MemoryContextSwitchTo(oldcontext);
 
-	if (!context->currMinipageValid)
+	for (;;)
 	{
-		Datum	minipage;
-		bool	minipageNull;
-
-		/* We need to fetch the next tuple from the blkdir relation */
-		if (!systable_getnext(context->scan))
-			goto srf_done;
-
-		/* deform the tuple and populate slot->values/nulls */
-		slot_getallattrs(context->scan->slot);
-
-		minipage = slot_getattr(context->scan->slot, Anum_pg_aoblkdir_minipage, &minipageNull);
-		/*
-		 * There should not really be any NULL values. We opt to report it
-		 * instead of ERRORing out.
-		 */
-		context->currMinipageValid = !minipageNull;
-		if (context->currMinipageValid)
+		if (!context->currMinipageValid)
 		{
+			Datum	minipage;
+			bool	minipageNull;
+
+			/* We need to fetch the next tuple from the blkdir relation */
+			if (!systable_getnext(context->scan))
+				break;
+
+			/* deform the tuple and populate slot->values/nulls */
+			slot_getallattrs(context->scan->slot);
+
+			minipage = slot_getattr(context->scan->slot, Anum_pg_aoblkdir_minipage, &minipageNull);
 			/*
-			 * Cache the latest scanned minipage and use it to emit the next
-			 * (context->currMinipage->numMinipageEntries) rows
+			 * There should not really be any NULL values. We opt to report it
+			 * instead of ERRORing out.
 			 */
-			copy_out_minipage(&context->currMinipage, minipage, false);
-			context->currMinipageEntryIdx = 0;
+			context->currMinipageValid = !minipageNull;
+			if (context->currMinipageValid)
+			{
+				/*
+				 * Cache the latest scanned minipage and use it to emit the next
+				 * (context->currMinipage->numMinipageEntries) rows
+				 */
+				copy_out_minipage(&context->currMinipage, minipage, false);
+				context->currMinipageEntryIdx = 0;
+			}
 		}
+
+		{
+			Datum          	values[7];
+			bool			nulls[7];
+			TupleTableSlot 	*slot = context->scan->slot;
+			Datum          	result;
+
+			values[0] = ItemPointerGetDatum(&slot->tts_tid);
+			nulls[0] = false;
+
+			values[1] = slot_getattr(slot, Anum_pg_aoblkdir_segno, &nulls[1]);
+			values[2] = slot_getattr(slot, Anum_pg_aoblkdir_columngroupno, &nulls[2]);
+
+			/* emit minipage entry */
+			if (context->currMinipageValid)
+			{
+				MinipagePerColumnGroup *currMinipage = &context->currMinipage;
+				MinipageEntry *minipageEntry;
+
+				Assert(context->currMinipageEntryIdx < currMinipage->numMinipageEntries);
+
+				minipageEntry = &currMinipage->minipage->entry[context->currMinipageEntryIdx];
+
+				values[3] = context->currMinipageEntryIdx++;
+				values[4] = Int64GetDatum(minipageEntry->firstRowNum);
+				values[5] = Int64GetDatum(minipageEntry->fileOffset);
+				values[6] = Int64GetDatum(minipageEntry->rowCount);
+
+				nulls[3] = false;
+				nulls[4] = false;
+				nulls[5] = false;
+				nulls[6] = false;
+
+				context->currMinipageValid =
+					(context->currMinipageEntryIdx != currMinipage->numMinipageEntries);
+			}
+			else
+			{
+				nulls[3] = true;
+				nulls[4] = true;
+				nulls[5] = true;
+				nulls[6] = true;
+			}
+
+			tuplestore_putvalues(tupstore, tupdesc, values, nulls);
+		}
+
+
 	}
 
-	{
-		Datum          	values[7];
-		bool			nulls[7];
-		TupleTableSlot 	*slot = context->scan->slot;
-		Datum          	result;
+	/* srf_done */
+	tuplestore_donestoring(tupstore);
 
-		values[0] = ItemPointerGetDatum(&slot->tts_tid);
-		nulls[0] = false;
-
-		values[1] = slot_getattr(slot, Anum_pg_aoblkdir_segno, &nulls[1]);
-		values[2] = slot_getattr(slot, Anum_pg_aoblkdir_columngroupno, &nulls[2]);
-
-		/* emit minipage entry */
-		if (context->currMinipageValid)
-		{
-			MinipagePerColumnGroup *currMinipage = &context->currMinipage;
-			MinipageEntry *minipageEntry;
-
-			Assert(context->currMinipageEntryIdx < currMinipage->numMinipageEntries);
-
-			minipageEntry = &currMinipage->minipage->entry[context->currMinipageEntryIdx];
-
-			values[3] = context->currMinipageEntryIdx++;
-			values[4] = Int64GetDatum(minipageEntry->firstRowNum);
-			values[5] = Int64GetDatum(minipageEntry->fileOffset);
-			values[6] = Int64GetDatum(minipageEntry->rowCount);
-
-			nulls[3] = false;
-			nulls[4] = false;
-			nulls[5] = false;
-			nulls[6] = false;
-
-			context->currMinipageValid =
-				(context->currMinipageEntryIdx != currMinipage->numMinipageEntries);
-		}
-		else
-		{
-			nulls[3] = true;
-			nulls[4] = true;
-			nulls[5] = true;
-			nulls[6] = true;
-		}
-
-		tuple = heap_form_tuple(funcctx->tuple_desc, values, nulls);
-		result = HeapTupleGetDatum(tuple);
-		SRF_RETURN_NEXT(funcctx, result);
-	}
-
-srf_done:
 	table_close(context->aorel, AccessShareLock);
 	systable_endscan(context->scan);
 	table_close(context->blkdirrel, AccessShareLock);
 	pfree(context);
-	funcctx->user_fctx = NULL;
-	SRF_RETURN_DONE(funcctx);
+
+	return (Datum) 0;
 }
