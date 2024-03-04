e970504c58af6cf0cf6cc5751b18a35f5491f4fb
Vasiliy Ivanov <ivi@arenadata.io>
2024-03-04 14:22:14 +0200

    Revert "Make InitPlans non-rescannable during estate initialization (#595)"
    to solve conflicts with 9290a5c
    
    This reverts commit ebd310c0854b8767e3b760b7f42124c167f6084b.

--- src/backend/gpopt/translate/CTranslatorDXLToPlStmt.cpp
+++ src/backend/gpopt/translate/CTranslatorDXLToPlStmt.cpp
@@ -280,7 +280,7 @@ CTranslatorDXLToPlStmt::GetPlannedStmtFromDXL(const CDXLNode *dxlnode,
 	//	pplstmt->intoClause = m_pctxdxltoplstmt->Pintocl();
 	planned_stmt->intoPolicy = m_dxl_to_plstmt_context->GetDistributionPolicy();
 
-	SetSubPlanVariables(planned_stmt);
+	SetInitPlanVariables(planned_stmt);
 
 	if (CMD_SELECT == m_cmd_type && NULL != dxlnode->GetDXLDirectDispatchInfo())
 	{
@@ -345,16 +345,14 @@ CTranslatorDXLToPlStmt::TranslateDXLOperatorToPlan(
 
 //---------------------------------------------------------------------------
 //	@function:
-//		CTranslatorDXLToPlStmt::SetSubPlanVariables
+//		CTranslatorDXLToPlStmt::SetInitPlanVariables
 //
 //	@doc:
 //		Iterates over the plan to set the qDispSliceId that is found in the plan
 //		as well as its subplans. Set the number of parameters used in the plan.
-//		Simultaneously fills rewindPlanIDs bitmapset in PlannedStmt with plan_id
-//		of all SubPlans, except InitPlans.
 //---------------------------------------------------------------------------
 void
-CTranslatorDXLToPlStmt::SetSubPlanVariables(PlannedStmt *planned_stmt)
+CTranslatorDXLToPlStmt::SetInitPlanVariables(PlannedStmt *planned_stmt)
 {
 	if (1 !=
 		m_dxl_to_plstmt_context
@@ -371,9 +369,6 @@ CTranslatorDXLToPlStmt::SetSubPlanVariables(PlannedStmt *planned_stmt)
 	List *subplan_list =
 		gpdb::ExtractNodesPlan(planned_stmt->planTree, T_SubPlan, true);
 
-	// set of plan_ids of any SubPlan except InitPLan
-	Bitmapset *planIds = NULL;
-
 	ListCell *lc = NULL;
 
 	ForEach(lc, subplan_list)
@@ -383,8 +378,6 @@ CTranslatorDXLToPlStmt::SetSubPlanVariables(PlannedStmt *planned_stmt)
 		{
 			SetInitPlanSliceInformation(subplan);
 		}
-		else
-			planIds = gpdb::BmsAddMember(planIds, subplan->plan_id);
 	}
 
 	// InitPlans can also be defined in subplans. We therefore have to iterate
@@ -405,12 +398,8 @@ CTranslatorDXLToPlStmt::SetSubPlanVariables(PlannedStmt *planned_stmt)
 			{
 				SetInitPlanSliceInformation(subplan);
 			}
-			else
-				planIds = gpdb::BmsAddMember(planIds, subplan->plan_id);
 		}
 	}
-
-	planned_stmt->rewindPlanIDs = planIds;
 }
 
 //---------------------------------------------------------------------------
